81f87d791e3c8dcf4f75781804d8852b9448e5ca
Brett Sun <qisheng.brett.sun@gmail.com>
2020-08-17 23:11:53 +0200

    Apply suggestions from code review
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

--- packages/controller/contracts/ANController.sol
+++ packages/controller/contracts/ANController.sol
@@ -25,7 +25,7 @@ contract ANTController is ITokenController {
         minter = _minter;
     }
 
-    function mintANT(address _owner, uint256 _amount) external onlyMinter returns (bool) {
+    function mint(address _owner, uint256 _amount) external onlyMinter returns (bool) {
         return ant.generateTokens(_owner, _amount);
     }
 
@@ -40,10 +40,20 @@ contract ANTController is ITokenController {
       return false;
     }
 
+    /**
+    * @dev Callback function called from MiniMe-like instances when an ERC20 transfer is requested
+    *      It allows specifying a custom logic to control if a transfer should be allowed or not
+    * @return Always true, this controller allows any ERC20 transfer
+    */
     function onTransfer(address /* _from */, address /* _to */, uint /* _amount */) external returns (bool) {
       return true;
     }
 
+    /**
+    * @dev Callback function called from MiniMe-like instances when an ERC20 approval is requested
+    *      It allows specifying a custom logic to control if an approval should be allowed or not
+    * @return Always true, this controller allows any ERC20 approval
+    */
     function onApprove(address /* _owner */, address /* _spender */, uint /* _amount */) external returns (bool) {
       return true;
     }
