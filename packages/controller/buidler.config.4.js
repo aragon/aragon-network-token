7116aa8cacb2868f18fcea422bd7217d5d7d5820
Brett Sun <qisheng.brett.sun@gmail.com>
2020-08-19 16:43:23 +0200

    Controller: add E2E tests (#15)

--- packages/controller/buidler.config.4.js
+++ packages/controller/buidler.config.4.js
@@ -1,5 +1,5 @@
 // This buidler configuration is only used to compile ANTMock.sol for testing,
-// as it relies on an older solc version (0.4.8)
+// which were deployed on an older solc version (0.4.8)
 module.exports = {
   solc: {
     version: "0.4.8",
