223dd828e677291dd7be7a510a9963a0fe55003e
Brett Sun <qisheng.brett.sun@gmail.com>
2020-09-05 12:16:18 +0200

    Controller: update buidler config and strip keys (#19)

--- packages/controller/buidler.config.js
+++ packages/controller/buidler.config.js
@@ -3,6 +3,9 @@ const { usePlugin } = require('@nomiclabs/buidler/config')
 usePlugin("@nomiclabs/buidler-ganache")
 usePlugin('@nomiclabs/buidler-truffle5')
 
+const ACCOUNTS = (process.env.ETH_KEYS ? process.env.ETH_KEYS.split(',') : [])
+  .map(key => key.trim())
+
 module.exports = {
   networks: {
     // Local development network using ganache. You can set any of the
@@ -17,18 +20,12 @@ module.exports = {
     // Mainnet network configured with Aragon node.
     mainnet: {
       url: 'https://mainnet.eth.aragon.network',
-      accounts: [
-        process.env.ETH_KEY ||
-          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
-      ],
+      accounts: ACCOUNTS,
     },
     // Rinkeby network configured with Aragon node.
     rinkeby: {
       url: 'https://rinkeby.eth.aragon.network',
-      accounts: [
-        process.env.ETH_KEY ||
-          '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
-      ],
+      accounts: ACCOUNTS,
     },
     // Network configured to interact with Frame wallet. Requires
     // to have Frame running on your machine. Download it from:
