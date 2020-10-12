8f9a94f1a3ab4f9a5e4d4ffc9ac014125d4c2fd4
Brett Sun <qisheng.brett.sun@gmail.com>
2020-10-12 17:13:21 +0200

    ANTv2: optimize transferFrom()'s allowance check

--- packages/v2/contracts/ANTv2.sol
+++ packages/v2/contracts/ANTv2.sol
@@ -126,9 +126,10 @@ contract ANTv2 is IERC20 {
     }
 
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
-        if (allowance[from][msg.sender] != uint256(-1)) {
+        uint256 fromAllowance = allowance[from][msg.sender];
+        if (fromAllowance != uint256(-1)) {
             // Allowance is implicitly checked with SafeMath's underflow protection
-            allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+            allowance[from][msg.sender] = fromAllowance.sub(value);
         }
         _transfer(from, to, value);
         return true;
