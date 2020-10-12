4aaa2145b9f12567e09f3a60d79dc46c2a3845fe
Brett Sun <qisheng.brett.sun@gmail.com>
2020-10-12 17:13:43 +0200

    ANTv2: cosmetic rewording, reordering

--- packages/v2/contracts/ANTv2.sol
+++ packages/v2/contracts/ANTv2.sol
@@ -16,20 +16,20 @@ contract ANTv2 is IERC20 {
     string public constant symbol = "ANT";
     uint8 public constant decimals = 18;
 
-    address public minter;
-    uint256 public totalSupply;
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping(address => uint256)) public allowance;
-
-    bytes32 public DOMAIN_SEPARATOR;
-
     // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    mapping (address => uint256) public nonces;
-
     // bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH =
     //     keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)");
     bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
+
+    address public minter;
+    uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    // ERC-712, ERC-2612, ERC-3009 state
+    bytes32 public DOMAIN_SEPARATOR;
+    mapping (address => uint256) public nonces;
     mapping (address => mapping (bytes32 => bool)) public authorizationState;
 
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -56,12 +56,12 @@ contract ANTv2 is IERC20 {
         _changeMinter(initialMinter);
     }
 
-    function _validateSignedData(address signer, bytes32 encodeData, uint8 v, bytes32 r, bytes32 s) internal view {
+    function _validateSignedData(address signer, bytes32 encodedData, uint8 v, bytes32 r, bytes32 s) internal view {
         bytes32 digest = keccak256(
             abi.encodePacked(
                 "\x19\x01",
                 DOMAIN_SEPARATOR,
-                encodeData
+                encodedData
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
@@ -138,8 +138,8 @@ contract ANTv2 is IERC20 {
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
         require(deadline >= block.timestamp, "ANTV2:AUTH_EXPIRED");
 
-        bytes32 encodeData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
-        _validateSignedData(owner, encodeData, v, r, s);
+        bytes32 encodedData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
+        _validateSignedData(owner, encodedData, v, r, s);
 
         _approve(owner, spender, value);
     }
@@ -161,8 +161,8 @@ contract ANTv2 is IERC20 {
         require(block.timestamp < validBefore, "ANTV2:AUTH_EXPIRED");
         require(!authorizationState[from][nonce],  "ANTV2:AUTH_ALREADY_USED");
 
-        bytes32 encodeData = keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce));
-        _validateSignedData(from, encodeData, v, r, s);
+        bytes32 encodedData = keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce));
+        _validateSignedData(from, encodedData, v, r, s);
 
         authorizationState[from][nonce] = true;
         emit AuthorizationUsed(from, nonce);
