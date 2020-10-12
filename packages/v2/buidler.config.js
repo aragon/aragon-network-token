26ccabb942dbb535ace3db2831a18329ce94b944
Brett Sun <qisheng.brett.sun@gmail.com>
2020-10-12 17:14:20 +0200

    ANTv2: add deployment task

--- packages/v2/buidler.config.js
+++ packages/v2/buidler.config.js
@@ -1,10 +1,15 @@
-const { usePlugin } = require('@nomiclabs/buidler/config')
+const { types, usePlugin } = require('@nomiclabs/buidler/config')
+const { deploy } = require('./buidler/cli')
 
 usePlugin("@nomiclabs/buidler-ganache")
 usePlugin('@nomiclabs/buidler-truffle5')
+usePlugin('buidler-local-networks-config-plugin')
 
-const ACCOUNTS = (process.env.ETH_KEYS ? process.env.ETH_KEYS.split(',') : [])
-  .map(key => key.trim())
+task('deploy', 'Deploy ANTv2 and migrator')
+  .addParam('owner', "The migrator's owner")
+  .addOptionalParam('chain', 'The chain ID to use', 1, types.int)
+  .addOptionalParam('antv1', 'The ANTv1 address to use', '0x960b236A07cf122663c4303350609A66A7B288C0')
+  .setAction(deploy)
 
 module.exports = {
   networks: {
@@ -20,12 +25,10 @@ module.exports = {
     // Mainnet network configured with Aragon node.
     mainnet: {
       url: 'https://mainnet.eth.aragon.network',
-      accounts: ACCOUNTS,
     },
     // Rinkeby network configured with Aragon node.
     rinkeby: {
       url: 'https://rinkeby.eth.aragon.network',
-      accounts: ACCOUNTS,
     },
     // Network configured to interact with Frame wallet. Requires
     // to have Frame running on your machine. Download it from:
