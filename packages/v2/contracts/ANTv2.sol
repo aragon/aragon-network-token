9c9871490140c0081412795340c7a2d570d84d36
Brett Sun <qisheng.brett.sun@gmail.com>
2020-10-15 00:25:37 +0200

    ANTv2: revert on transfers to address(0) (#26)

--- packages/v2/contracts/ANTv2.sol
+++ packages/v2/contracts/ANTv2.sol
@@ -93,7 +93,7 @@ contract ANTv2 is IERC20 {
     }
 
     function _transfer(address from, address to, uint256 value) private {
-        require(to != address(this), "ANTV2:RECEIVER_IS_TOKEN");
+        require(to != address(this) && to != address(0), "ANTV2:RECEIVER_IS_TOKEN_OR_ZERO");
 
         // Balance is implicitly checked with SafeMath's underflow protection
         balanceOf[from] = balanceOf[from].sub(value);
